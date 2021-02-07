e41c8852ac7b511997ec0ab67ee2ff0bf12c3abb
Oniris <72936212+Oniris@users.noreply.github.com>
2021-02-07 16:31:30 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/SuzanFrogg/PtutS4_Santee into main""
    This reverts commit 5f22728c338e39759c2800119e9b79cc03f66caa.

--- Application/server/controllers/auth.controller.js
+++ Application/server/controllers/auth.controller.js
@@ -1,4 +1,14 @@
 import userModel from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const maxAge = 3 * 24 * 60 * 60 * 1000;//3 jours
+
+const createToken = (id) => {
+	return jwt.sign({id}, process.env.TOKEN_SECRET, {
+		expiresIn: maxAge
+	});
+}
+
 
 /**
  * Inscris un utilisateur (dans la bdd)
@@ -11,9 +21,34 @@ let signUp = async (req, res) => {
 		const user = await userModel.create({pseudo, email, password});
 		res.status(201).json({ user: user._id });
 	}
-	catch(err) {
-		res.status(400).send({ err });
+	catch (err) {
+		res.status(400).json({ error: "ID inconnu" });
 	}
 }
 
-export default {signUp};
+/**
+ * Permet de se connecter
+ */
+let login = async (req, res) => {
+	const {email, password} = req.body;
+
+	try {
+		const user = await userModel.login(email, password);
+		const token = createToken(user._id);
+		res.cookie("jwt", token, { httpOnly: true, maxAge });
+		res.status(200).json({ user: user._id });
+	}
+	catch (err) {
+		res.status(400).json({ error: err });
+	}
+}
+
+/**
+ * Permet de se déconnecter
+ */
+let logout = async (req, res) => {
+	res.cookie("jwt", "", { maxAge: 1 });
+	res.redirect("/");
+}
+
+export default {signUp, login, logout};
