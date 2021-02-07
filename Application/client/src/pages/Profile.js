54582fdfe35ab91b3b2f7d61cd458a3e707214c1
Oniris <72936212+Oniris@users.noreply.github.com>
2021-02-07 16:28:28 +0100

    Création des différentes pages

---
+++ Application/client/src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div>
+			<h1>Votre profil</h1>
+		</div>
+	);
+}
+
+export default Header;
---
+++ Application/client/src/pages/Stats.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div>
+			<h1>Vos statistiques</h1>
+		</div>
+	);
+}
+
+export default Header;
