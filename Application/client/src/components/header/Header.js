feec3aafc31d4bf1dfe35e75c6bc2a5a28904d9f
Oniris <72936212+Oniris@users.noreply.github.com>
2021-02-07 18:12:58 +0100

    Update Header.js

--- Application/client/src/components/header/Header.js
+++ Application/client/src/components/header/Header.js
@@ -3,7 +3,20 @@ import React from "react";
 function Header() {
 	return (
 		<header>
-			Test du header
+			<nav>
+				<div>
+					<img src={''} alt="logo"/> 
+				</div>
+				<div>
+					<ul>
+						<li><a href="/">Accueil</a></li>
+						<li><a href="../../pages/Stats">Statistiques</a></li>
+						<li><a href="../../pages/Calendar">Calendrier</a></li>
+						<li><a href="../../pages/Objectives">Objectifs</a></li>
+						<li><a href="../../pages/Pofile">Profil</a></li>
+					</ul>
+				</div>
+			</nav>
 		</header>
 	);
 }
