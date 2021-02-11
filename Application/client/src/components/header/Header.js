d7db3a8ff53377faa8401536169e112e1f3bc8cc
Spicat <trembleau.thibault@gmail.com>
2021-02-11 02:18:31 +0100

    header disposition et ajout d’icônes

--- Application/client/src/components/header/Header.js
+++ Application/client/src/components/header/Header.js
@@ -1,22 +1,27 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import HeaderItem from "./HeaderItem";
+import { ReactComponent as Home } from "../../media/icons/fi-home.svg";
+import { ReactComponent as Stats } from "../../media/icons/fi-stats.svg";
+import { ReactComponent as Calendar } from "../../media/icons/fi-calendar.svg";
+import { ReactComponent as Objectives } from "../../media/icons/fi-badge.svg";
 
 function Header() {
 	return (
 		<header>
-			<div>
-				<div>
-					<img src="" alt="logo"/> 
-				</div>
-				<nav>
-					<ul>
-						<HeaderItem link="/" text="Accueil" />
-						<HeaderItem link="/Stats" text="Statistiques" />
-						<HeaderItem link="/Calendar" text="Calendrier" />
-						<HeaderItem link="/Objectives" text="Objectifs" />
-						<HeaderItem link="/Profile" text="Profil" />
-					</ul>
-				</nav>
+			<div className="header-logo">
+				<img src="" alt="logo"/> 
+			</div>
+			<ul className="header-menu">
+				<HeaderItem link="/" text="Accueil" icon={<Home />} />
+				<HeaderItem link="/Stats" text="Statistiques" icon={<Stats />} />
+				<HeaderItem link="/Calendar" text="Calendrier" icon={<Calendar />} />
+				<HeaderItem link="/Objectives" text="Objectifs" icon={<Objectives />} />
+			</ul>
+			<div className="profil-menu">
+				<Link to="/Profile">
+					<img src="./uploads/profil/default-user.jpg" alt="Profil"/>
+				</Link>
 			</div>
 		</header>
 	);
--- Application/client/src/components/header/HeaderItem.js
+++ Application/client/src/components/header/HeaderItem.js
@@ -4,7 +4,8 @@ import {NavLink} from "react-router-dom";
 function HeaderItem(props) {
 	return (
 		<li>
-			<NavLink className="header-link" exact activeClassName="header-active" to={props.link}>
+			<NavLink className="header-link" activeClassName="header-active" exact to={props.link}>
+				{props.icon}
 				<span>{props.text}</span>
 			</NavLink>
 		</li>
