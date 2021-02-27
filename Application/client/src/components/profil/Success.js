9758a7cf36b14b3457ab405fe2d7163492190a08
SuzanFrogg <33319686+SuzanFrogg@users.noreply.github.com>
2021-02-27 12:39:51 +0100

    Profil

---
+++ Application/client/src/components/profil/Success.js
@@ -0,0 +1,17 @@
+import React from "react";
+import axios from "axios";
+
+function Success() {
+
+
+	return (
+
+        <div className="succesP">
+            <h3>Succès</h3>
+
+        </div>
+
+	);
+}
+
+export default Success;
