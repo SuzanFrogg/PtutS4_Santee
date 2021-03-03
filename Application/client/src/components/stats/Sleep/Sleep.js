b06e59a98da0176f1747ed3de3775cb1cba7c4f2
0rega <72932562+0rega@users.noreply.github.com>
2021-03-03 10:26:01 +0100

    graph update

--- Application/client/src/components/stats/Sleep/Sleep.js
+++ Application/client/src/components/stats/Sleep/Sleep.js
@@ -7,12 +7,12 @@ function Sleep(){
 	let dataChartBar = {
 			//axe des abscisses
 			datasets: [{
-				label: 'Heure de sommeil',
+				label: "Heure de sommeil",
+				
+				labels: ["Lundi", 'Mardi', 'Mercredi', 'Vendredi', 'Samedi', 'Dimanche'],
+
 				//données
 				data: [{
-					t: 'Feb 21 2021',
-					y: 5
-				}, {
 					t: 'Feb 22 2021',
 					y: 8
 				}, {
@@ -24,47 +24,65 @@ function Sleep(){
 				}, {
 					t: 'Feb 25 2021',
 					y: 8
+				}, {
+					t: 'Feb 26 2021',
+					y: 9
+				}, {
+					t: 'Feb 27 2021',
+					y: 8
+				}, {
+					t: 'Feb 28 2021',
+					y: 9
 				}],
 				backgroundColor:'rgb(50,144,255)',
 				borderWidth:1,
 				borderColor: '#777',
 				hoverBorderColor:'#000',
-				hoverBorderWidth:3,
+				hoverBorderWidth:3
 			}]
 		};
 
 	//Option graphique sommeil
 	let optionChartBar = {
-			responsive: true,
-			title: {
-				display: true,
-				text: "Nombre d'heures de sommeil"
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
-			},
-			hover: {
-				mode: 'nearest',
-				intersect: true
-			},
-			scales: {
-				xAxes: [{
-					type: 'time',
-					time: {
-						unit: 'week'
-					}
-				}],
-				yAxes: [{
-					ticks: {
-						min: 0,
-						max: 12
-					}
-				}]
-			}
+		responsive: true,
+		title: {
+			display: true,
+			text: "Nombre d'heures de sommeil",
+			fontSize: 30,
+			fontColor: 'rgb(0,0,0)'
+		},
+		tooltips: {
+			mode: 'index',
+			intersect: false
+		},
+		hover: {
+			mode: 'nearest',
+			intersect: true
+		},
+		legend: {
+			position: 'bottom'
+		},
+		scales: {
+			xAxes: [{
+				type: 'time',
+				time: {
+					//Affiche un jour
+					unit: 'day'
+				},
+				ticks: {
+					display: true
+				},
+				categoryPercentage: 0.5
+			}],
+			yAxes: [{
+				ticks: {
+					beginAtZero: true,
+					max: 12
+				}
+			}]
+		}		
 	};	
 
-	
     return (
 		<div>
 			<h2>Sommeil</h2>
@@ -87,7 +105,7 @@ function Sleep(){
 					<p>données</p>
 				</div>
 				<div className="data-case">
-					<h3>Fluctuation moyenne</h3>
+					<h3>Fluctuation moyenne du sommeil</h3>
 					<p>données</p>
 				</div>						
 			</div>
