8c0d0f0f226c128bed35cd05d1d184b51dc520bf
0rega <72932562+0rega@users.noreply.github.com>
2021-03-06 15:07:22 +0100

    Update sleep.controller.js

--- Application/server/controllers/sleep.controller.js
+++ Application/server/controllers/sleep.controller.js
@@ -16,14 +16,13 @@ let getSleep = async (req, res) => {
  * Permet de créer une donnée de sleep
  */
 let createSleep = async (req, res) => {
-    let newDataSleep = new sleepNestedSchema({
-        dateStart: req.body.dateStart,
-        dateEnd: req.body.dateEnd
-    });
 
     let newSleep = new sleepModel({
         idUser: req.body.idUser,
-        Sleep: newDataSleep
+        Sleep: {
+        dateStart: req.body.dateStart,
+        dateEnd: req.body.dateEnd
+    }
     });
 
     try {
@@ -37,17 +36,45 @@ let createSleep = async (req, res) => {
 };
 
 /**
- * Permet de modifier les données de sleep
+ * Permet de modifier les donnée de sleep
  */
-let updateSleep = (req, res) => {
+let updateSleep =   async (req, res) => {
+    //Vérifie l'id
+    if(!mongoose.isValidObjectId(req.params.id))
+        return res.status(400).send("ID unknown : " + req.params.id);
 
+    try {
+		await sleepModel.findOneAndUpdate(
+            {idUser: req.params.id},
+			{
+				$set: {
+					dateStart: req.body.dateStart,
+					dateEnd: req.body.dateEnd
+				}
+			},
+			{ new: true},
+			(err, docs) => {
+				if (!err) return res.status(200).json(docs);
+				else return res.status(500).json({ error: err });
+			}
+		)
+	}
+	catch (err) {
+		return res.status(500).json({ error: err });
+	}
 };
 
 /**
  * Permet de supprimer une donnée de sleep
  */
 let deleteSleep = (req, res) => {
+    if(!mongoose.isValidObjectId(req.params.id))
+        return res.status(400).send("ID unknown : " + req.params.id);
 
+	sleepModel.findByIdAndRemove(req.params.id, (err, docs) => {
+        if(!err) res.send(docs);
+        else console.log("Delete error : " + err);
+    });
 };
 
 export default {getSleep, createSleep, updateSleep, deleteSleep};
