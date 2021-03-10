f0adffb69fd83e15468e87784dcba99feb7a370e
albane-maublanc <albane-m@live.fr>
2021-03-10 18:16:57 +0100

    backend pour les règles

---
+++ Application/server/models/periods.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const periodsNestedSchema = mongoose.Schema({
+	dateStart: {
+		type: Date,
+		required: true,
+		default: Date.now
+	},
+	dateEnd: {
+		type: Date,
+		required: true,
+		default: Date.now
+	},
+    flux : {
+        type: Number,
+        required: true
+    }
+});
+
+const periodsSchema = mongoose.Schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		unique: true,
+		required: true
+	},
+	Periods: [periodsNestedSchema]
+});
+
+export default mongoose.model("periods", periodsSchema);
---
+++ Application/server/routes/periods.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+import periodsController from "../controllers/periods.controller.js";
+
+router.get("/", periodsController.getPeriods); //READ
+router.post('/', periodsController.createPeriods); //CREATE
+router.put("/:periodsId", periodsController.updatePeriods); //UPDATE
+router.put("/", periodsController.addPeriods); //UPDATE
+router.delete("/:periodsId", periodsController.deletePeriods); //DELETE
+
+export default router;
