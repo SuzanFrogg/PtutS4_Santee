bba888e09ebf64f0317f0d9eb62b91452d362145
Oniris <72936212+Oniris@users.noreply.github.com>
2021-03-20 15:39:12 +0100

    Calcul menstruation
    J'ai commencé les calculs mais ils ne sont pas finis.

--- Application/client/src/components/stats/Menstruation/Menstruation.js
+++ Application/client/src/components/stats/Menstruation/Menstruation.js
@@ -1,26 +1,64 @@
 import React from "react";
+import axios from "axios";
 import MenstruationAdd from "./Menstruation_add";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function Menstruation()
 {
     const [showAddForm, setShowAddForm] = useState(false);
+
+	let userPeriods = [];
+	let dateStartP = [];
+	let dateEndP = [];
+
+	//se lance a chaque chargement
+	useEffect(() => {
+		let isMounted = true;
+
+		//Récupération des informations de don
+		const fetchDon = async () =>
+		{
+			const dataDons = await axios.get('/api/periods/', {withCredentials: true});
+			if (isMounted) {
+				userPeriods = dataDons.data.periods;
+				for (let i=0; i<(userPeriods.length); i++) {
+					dateStartP = userPeriods[i].dateStart;
+					dateEndP = userPeriods[i].dateEnd;
+				}
+			}
+		}
+
+		fetchDon();
+		return () => { isMounted = false };
+	});
+
+	const getMoyCycle = (periods, dateStart, dateEnd) => {
+		let moy = 0;
+		return moy;
+	}
+
+	const getMoyRegle = (periods, dateStart, dateEnd) => {
+		let moy = 0;
+		let dS = dateStart;
+		let dE = dateEnd;
+		for (let i=0; i<(periods.length); i++) {
+			moy += (dS[i] - dE[i]);
+		}
+		return moy;
+	}
+
     return (
 		<>
 			<h2>Règles</h2>
 			<div className="data-recap">
 				<div className="data-card">
 					<p>Durée moyenne du cycle</p>
-					<span>28<small>j</small></span>
+					<span>{getMoyCycle(userPeriods, dateStartP, dateEndP)}<small>j</small></span>
 				</div>
 				<div className="data-card">
 					<p>Durée moyenne des règles</p>
-					<span>28<small>j</small></span>
-				</div>
-				<div className="data-card">
-					<p>Fluctuation moyenne du cycle</p>
-					<span>28<small>j</small></span>
-				</div>						
+					<span>{getMoyRegle(userPeriods, dateStartP, dateEndP)}<small>j</small></span>
+				</div>					
 			</div>
 			<div>
 				<button onClick={(event) => setShowAddForm(true)}> Ajouter </button>
